7596ae30f70ac6602b7206a418d0f87cd6c78396
Akonaay <ambrosesulley4@gmail.com>
2021-08-03 15:39:34 +0300

    UI for About & Services

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,4 +16,20 @@
         }
       }
     })
+
+    //  Mission/Vision Accordion
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("pactive");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
   })
