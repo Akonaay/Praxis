f6d06699e8af3c375d735dcd124a41ae81d0e58c
akonnay <ambrosesulley4@gmail.com>
2021-12-11 13:44:26 +0300

    UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,53 @@
-  $(document).ready(function () {
-    $("#owl-agency").owlCarousel({
-      loop: true,
-      nav: false,
-      autoplay: true,
-      margin: 50,
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1,
-          nav: false
+$(document).ready(function () {
+  $("#owl-agency").owlCarousel({
+    loop: true,
+    nav: false,
+    autoplay: true,
+    autospeed: 10,
+    margin: 50,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      736: {
+        items: 1,
+        nav: false,
+      },
+    }, 
+  });
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+      0: {
+          items: 1
         },
-        736: {
-          items: 1,
-          nav: false
+        767: {
+          items: 2
+        },
+        1000: {
+          items: 3
         }
+    },
+  });
+
+  //  Mission/Vision Accordion
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("pactive");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
       }
-    })
-
-    //  Mission/Vision Accordion
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        this.classList.toggle("pactive");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-      });
-    }
-  })
+    });
+  }
+});
---
+++ precache-manifest.a5d1c87c571fabddab3b69319e6bc3f7.js
@@ -0,0 +1,474 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bab4578692993514e7f882cc15c218",
+    "url": "/assets/css/all.min.css"
+  },
+  {
+    "revision": "3d11a5022ba581776073ce64bed65dee",
+    "url": "/assets/css/app.css"
+  },
+  {
+    "revision": "d432e4222814b62dd30c9513dcc29440",
+    "url": "/assets/css/bootstrap.min.css"
+  },
+  {
+    "revision": "b2752a850d44f50036628eeaef3bfcfa",
+    "url": "/assets/css/owl.carousel.min.css"
+  },
+  {
+    "revision": "af7cc13885688a952fa840ede6b24fef",
+    "url": "/assets/images/1-Slide-1.jpg"
+  },
+  {
+    "revision": "c4a58bfa55785d79370667fdb1249050",
+    "url": "/assets/images/Banners/1-Slide-1.jpg"
+  },
+  {
+    "revision": "c4b08cb3c4d6baee332077ac72407473",
+    "url": "/assets/images/Banners/2-Slide-2.jpg"
+  },
+  {
+    "revision": "6651ac85149886d454b0f759c3bab87b",
+    "url": "/assets/images/Banners/3-Slide-3.jpg"
+  },
+  {
+    "revision": "04828f37e23488642e61794caf4e6209",
+    "url": "/assets/images/Banners/Slide1.jpg"
+  },
+  {
+    "revision": "47ade0842a580d9b1c4723223fd0b7de",
+    "url": "/assets/images/Banners/Slide2.jpg"
+  },
+  {
+    "revision": "91fdbd02b21246de603f3d81c04802ac",
+    "url": "/assets/images/Banners/Slide3.jpg"
+  },
+  {
+    "revision": "af7cc13885688a952fa840ede6b24fef",
+    "url": "/assets/images/Banners/Slide4.jpg"
+  },
+  {
+    "revision": "b9c462923a1353689b53fdc17fb47cdd",
+    "url": "/assets/images/Banners/about-us.jpg"
+  },
+  {
+    "revision": "990dc6a09015f56e6e13ce08b6062ea8",
+    "url": "/assets/images/Banners/bg1.jpg"
+  },
+  {
+    "revision": "16be3a5e10893db388b2719f0874bb7c",
+    "url": "/assets/images/Banners/bg2.jpg"
+  },
+  {
+    "revision": "933ae0ff05e7101dee1dbc9e41129260",
+    "url": "/assets/images/Banners/bg3.jpg"
+  },
+  {
+    "revision": "51b31dfd78a0d63e1190ca8fb4c3679c",
+    "url": "/assets/images/Banners/services.jpg"
+  },
+  {
+    "revision": "fcb4267d8189570a5d08832d17a3d484",
+    "url": "/assets/images/Features/banner1.png"
+  },
+  {
+    "revision": "5ebb9db03bb8aeeb4710db976f0387ad",
+    "url": "/assets/images/Features/p1.jpg"
+  },
+  {
+    "revision": "c74c86a0c12179a380d9877a64b7e75e",
+    "url": "/assets/images/Features/p10.jpg"
+  },
+  {
+    "revision": "2df9742d5a37b8775cf850bb1015d38b",
+    "url": "/assets/images/Features/p11.jpg"
+  },
+  {
+    "revision": "0b0e747ed7e0867507310e6946cd1be5",
+    "url": "/assets/images/Features/p12.jpg"
+  },
+  {
+    "revision": "202537bcaa8c52da05df6e0917d1b4f9",
+    "url": "/assets/images/Features/p13.jpg"
+  },
+  {
+    "revision": "fb9966235b1c3e18d56250b6af7ae3e7",
+    "url": "/assets/images/Features/p14.jpg"
+  },
+  {
+    "revision": "be4f1553274dfb9e602e7551ae2c9519",
+    "url": "/assets/images/Features/p2.jpg"
+  },
+  {
+    "revision": "65b711d8fe75b6a9be273eca2450e1f6",
+    "url": "/assets/images/Features/p3.jpg"
+  },
+  {
+    "revision": "e5720721bee594102065b6afd5b59319",
+    "url": "/assets/images/Features/p4.jpg"
+  },
+  {
+    "revision": "bc2b5b21bafb0504a6041096e31f58c1",
+    "url": "/assets/images/Features/p5.jpg"
+  },
+  {
+    "revision": "efc7168ca053c9cc51d2acc5d9ccac51",
+    "url": "/assets/images/Features/p6.jpg"
+  },
+  {
+    "revision": "f65967fe6d0031a0a8b8972649f37342",
+    "url": "/assets/images/Features/p7.jpg"
+  },
+  {
+    "revision": "2f57c3d49c6d4271e4996c8c339a7360",
+    "url": "/assets/images/Features/p8.jpg"
+  },
+  {
+    "revision": "513a1c1b093f072ca2aafed557fd4afb",
+    "url": "/assets/images/Features/p9.jpg"
+  },
+  {
+    "revision": "6100a18f5d9cfe408c850921787776d2",
+    "url": "/assets/images/PHD Website Content 2.docx"
+  },
+  {
+    "revision": "e6ceafc1894067e98d1c52a0d2154268",
+    "url": "/assets/images/Pages/1-Slide-1.jpg"
+  },
+  {
+    "revision": "bdce9cd00293f2d11ea0feec29cc5e58",
+    "url": "/assets/images/Pages/2-Slide-2.jpg"
+  },
+  {
+    "revision": "9b0dba6c0afb2eaf7458db747207cd87",
+    "url": "/assets/images/Pages/3-Slide-3.jpg"
+  },
+  {
+    "revision": "78c13f2c6e8d085a7568443b90516da8",
+    "url": "/assets/images/Pages/4-Slide-4.jpg"
+  },
+  {
+    "revision": "516fe70258c48d2dbd39e0f7f343f091",
+    "url": "/assets/images/Pages/5-Slide-1-copy-4.jpg"
+  },
+  {
+    "revision": "438389a487d2342b84c36f51c9d74064",
+    "url": "/assets/images/Pages/6-Slide-1-copy-5.jpg"
+  },
+  {
+    "revision": "7414d465b939d176312256fdde9e5c47",
+    "url": "/assets/images/Website content_PHD_Draft.docx"
+  },
+  {
+    "revision": "d1fdff9ac8418397e1d0ddc7d0357c25",
+    "url": "/assets/images/ab2.jpg"
+  },
+  {
+    "revision": "cfd2e5850c1bb4aa46a1f9983605a42f",
+    "url": "/assets/images/ab6.jpg"
+  },
+  {
+    "revision": "16f723fddb52c37a4db52312123f884e",
+    "url": "/assets/images/ab8.jpg"
+  },
+  {
+    "revision": "88c0412d8502a4e845ae9b5ae79d4dbe",
+    "url": "/assets/images/about.jpg"
+  },
+  {
+    "revision": "8cc5df0392257f7e7e3fdc3740b71015",
+    "url": "/assets/images/admin.jpg"
+  },
+  {
+    "revision": "0635c62c70819881451a523343b81c29",
+    "url": "/assets/images/building.jpg"
+  },
+  {
+    "revision": "37c93b3cc7225b13b6248d96a393401c",
+    "url": "/assets/images/capacity.jpg"
+  },
+  {
+    "revision": "4db6d2f9fe72fa2f5b3200ac5eeb842f",
+    "url": "/assets/images/carmma.png"
+  },
+  {
+    "revision": "8b42da2c919ba1845db1ca353b27195c",
+    "url": "/assets/images/contact-bn.jpg"
+  },
+  {
+    "revision": "98017ca259f9247268e25f61d110fe70",
+    "url": "/assets/images/digital.jpg"
+  },
+  {
+    "revision": "cdc2d3e9cdcb448683f8aeebcd5508e2",
+    "url": "/assets/images/feature.jpg"
+  },
+  {
+    "revision": "d7170fb9fb37e44f0ca894595af854bb",
+    "url": "/assets/images/leadership.jpg"
+  },
+  {
+    "revision": "657a10c3c7e0d68f4175977ec3ac1460",
+    "url": "/assets/images/logo.png"
+  },
+  {
+    "revision": "e12516b939b0250682f344a5d7c9b2f8",
+    "url": "/assets/images/overview-bg.png"
+  },
+  {
+    "revision": "c6a0990a6beec3eb7ac47ddb1e28932e",
+    "url": "/assets/images/pexels-miguel-á-padriñán-194094(1).jpg"
+  },
+  {
+    "revision": "a781a8f0e345416cd401623cd5863978",
+    "url": "/assets/images/policy.jpg"
+  },
+  {
+    "revision": "52621cc7c9c4a5f5ea44180547d2976a",
+    "url": "/assets/images/publication-0.jpg"
+  },
+  {
+    "revision": "14070b5c86a9936378977df019b7a27b",
+    "url": "/assets/images/publication-1.jpg"
+  },
+  {
+    "revision": "52621cc7c9c4a5f5ea44180547d2976a",
+    "url": "/assets/images/publication.jpg"
+  },
+  {
+    "revision": "85577a6f134342cc5f51ddf9284b2268",
+    "url": "/assets/images/research.jpg"
+  },
+  {
+    "revision": "72accac0c11e5c37db5661c0d1f232b0",
+    "url": "/assets/images/s1 (1).jpg"
+  },
+  {
+    "revision": "87b00978d4535334c6537ff5e4ef281b",
+    "url": "/assets/images/s1.jpg"
+  },
+  {
+    "revision": "117b782cf4ebbcbe1a68ae3bb669be42",
+    "url": "/assets/images/s2 (1).jpg"
+  },
+  {
+    "revision": "a2fc59971a345e057af599e7862ccb1c",
+    "url": "/assets/images/s2.jpg"
+  },
+  {
+    "revision": "1eb4d4d9130639b62546102235637d54",
+    "url": "/assets/images/s3 (1).jpg"
+  },
+  {
+    "revision": "27f5e54184a31b7f30f5cd3aa88205e0",
+    "url": "/assets/images/s5 (1).jpg"
+  },
+  {
+    "revision": "85577a6f134342cc5f51ddf9284b2268",
+    "url": "/assets/images/service-1 (1).jpg"
+  },
+  {
+    "revision": "1636ff63958b1b747d5c34d1329086ea",
+    "url": "/assets/images/service-10.jpg"
+  },
+  {
+    "revision": "98017ca259f9247268e25f61d110fe70",
+    "url": "/assets/images/service-11.jpg"
+  },
+  {
+    "revision": "0635c62c70819881451a523343b81c29",
+    "url": "/assets/images/service-2 (1).jpg"
+  },
+  {
+    "revision": "37c93b3cc7225b13b6248d96a393401c",
+    "url": "/assets/images/service-3 (1).jpg"
+  },
+  {
+    "revision": "369588a453c08cbf3239ccb660298647",
+    "url": "/assets/images/service-4.jpg"
+  },
+  {
+    "revision": "7264183a42ba1ae7d9ba9946529a9109",
+    "url": "/assets/images/service-5.jpg"
+  },
+  {
+    "revision": "7bbfebc7af7ad7b5089ad1c112c19146",
+    "url": "/assets/images/service-6.jpg"
+  },
+  {
+    "revision": "d7170fb9fb37e44f0ca894595af854bb",
+    "url": "/assets/images/service-7.jpg"
+  },
+  {
+    "revision": "b0183df81f600fbf7d115a2adb67e4f0",
+    "url": "/assets/images/service-8.jpg"
+  },
+  {
+    "revision": "237c4f12b8918ea256a08b79efad9691",
+    "url": "/assets/images/service-9.jpg"
+  },
+  {
+    "revision": "8197de9a003872532a754ed02d6cbe54",
+    "url": "/assets/images/services.jpg"
+  },
+  {
+    "revision": "db053a5e3c0883faa784320a21975da6",
+    "url": "/assets/images/singleblog.jpg"
+  },
+  {
+    "revision": "b0dce148022badda07b762da5ee29522",
+    "url": "/assets/images/slide2.jpg"
+  },
+  {
+    "revision": "982aaffd5d706cb2262bb9dc1e323c4f",
+    "url": "/assets/images/slide3.jpg"
+  },
+  {
+    "revision": "26f6dbf48bdb9328602d451eb3c9c0d3",
+    "url": "/assets/images/slide4.jpg"
+  },
+  {
+    "revision": "6a8774dc9604885b76d5f51507d84d4f",
+    "url": "/assets/images/slide5.jpg"
+  },
+  {
+    "revision": "6fa36bbbfb3ffcd75da386723b2df738",
+    "url": "/assets/images/slide6.jpg"
+  },
+  {
+    "revision": "cdaf64305addfd549c557648b94a377f",
+    "url": "/assets/images/team/dastan.jpg"
+  },
+  {
+    "revision": "d89746888da2d9510b64a9f031eaecd5",
+    "url": "/assets/images/team/sample.gif"
+  },
+  {
+    "revision": "0763b1034cbfde53b6e87072c203aed7",
+    "url": "/assets/images/team/sample.png"
+  },
+  {
+    "revision": "5582afafd153185ba7fdb644759d19e8",
+    "url": "/assets/images/team/venne.jpg"
+  },
+  {
+    "revision": "088b9929c536ead386ea4e4e7a937fe6",
+    "url": "/assets/images/team1.jpg"
+  },
+  {
+    "revision": "3ecc0fba739936fe248ccc71989fa471",
+    "url": "/assets/images/team2.jpg"
+  },
+  {
+    "revision": "f8d77550fa0b195ee590aa23ebbeaa23",
+    "url": "/assets/images/team3.jpg"
+  },
+  {
+    "revision": "189094bda050e25481cc30855b6d84b9",
+    "url": "/assets/images/team4.jpg"
+  },
+  {
+    "revision": "53ace5af4f2c43c17cd651511fcc3804",
+    "url": "/assets/images/testi1.jpg"
+  },
+  {
+    "revision": "29290d901ae449ce555bb5a06456825b",
+    "url": "/assets/images/testi2.jpg"
+  },
+  {
+    "revision": "2a1f7943f331a7605637beb5681e3e20",
+    "url": "/assets/images/testi3.jpg"
+  },
+  {
+    "revision": "836b021922a9fb4f54fafb01c7f105f7",
+    "url": "/assets/js/app.js"
+  },
+  {
+    "revision": "90146f01d8a2028ed6f2c3d2fba4ac9b",
+    "url": "/assets/js/bootstrap.bundle.min.js"
+  },
+  {
+    "revision": "f0c2bcf5ef0c4476508d79ec9cdcce07",
+    "url": "/assets/js/bootstrap.min.js"
+  },
+  {
+    "revision": "fb8409a092adc6e8be17e87d59e0595e",
+    "url": "/assets/js/jquery-3.5.1.slim.min.js"
+  },
+  {
+    "revision": "f416f9031fef25ae25ba9756e3eb6978",
+    "url": "/assets/js/owl.carousel.min.js"
+  },
+  {
+    "revision": "1022eaf388cc780bcfeb6456157adb7d",
+    "url": "/assets/js/popper.min.js"
+  },
+  {
+    "revision": "0635c62c70819881451a523343b81c29",
+    "url": "/img/building.0635c62c.jpg"
+  },
+  {
+    "revision": "37c93b3cc7225b13b6248d96a393401c",
+    "url": "/img/capacity.37c93b3c.jpg"
+  },
+  {
+    "revision": "98017ca259f9247268e25f61d110fe70",
+    "url": "/img/digital.98017ca2.jpg"
+  },
+  {
+    "revision": "cdc2d3e9cdcb448683f8aeebcd5508e2",
+    "url": "/img/feature.cdc2d3e9.jpg"
+  },
+  {
+    "revision": "d7170fb9fb37e44f0ca894595af854bb",
+    "url": "/img/leadership.d7170fb9.jpg"
+  },
+  {
+    "revision": "657a10c3c7e0d68f4175977ec3ac1460",
+    "url": "/img/logo.657a10c3.png"
+  },
+  {
+    "revision": "a781a8f0e345416cd401623cd5863978",
+    "url": "/img/policy.a781a8f0.jpg"
+  },
+  {
+    "revision": "52621cc7c9c4a5f5ea44180547d2976a",
+    "url": "/img/publication.52621cc7.jpg"
+  },
+  {
+    "revision": "85577a6f134342cc5f51ddf9284b2268",
+    "url": "/img/research.85577a6f.jpg"
+  },
+  {
+    "revision": "53ace5af4f2c43c17cd651511fcc3804",
+    "url": "/img/testi1.53ace5af.jpg"
+  },
+  {
+    "revision": "1e5950a34914f95c8cb30c11bf95aef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7746d89c98dc113d8325",
+    "url": "/js/about.eefe26ee.js"
+  },
+  {
+    "revision": "4afe3992617af9e3ad4f",
+    "url": "/js/app.35069712.js"
+  },
+  {
+    "revision": "c07ca343863e525dfced",
+    "url": "/js/chunk-vendors.6c0724be.js"
+  },
+  {
+    "revision": "657a10c3c7e0d68f4175977ec3ac1460",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "59ff7b16602e21d351a109c14bcd7701",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
